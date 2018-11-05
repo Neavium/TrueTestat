b859d4ea2a39f54c61e40e34505bab86d8022015
Kuma <michael-naef@hotmail.com>
2018-11-05 22:07:00 +0100

    dummy actions for top buttons (controller and routing done)

---
+++ controller/indexController.js
@@ -0,0 +1,29 @@
+
+
+module.exports.showIndex = function (req, res) {
+    res.render('index', { title: 'TrueTestat' });
+};
+
+module.exports.createNote = function (req, res) {
+    res.send("Create Note: Not implemented yet, sorry")
+}
+
+module.exports.styleSwitch = function (req, res){
+    res.send("Style Switch: Not implemented yet, sorry")
+}
+
+module.exports.sortFinishDate = function (req, res){
+    res.send("Sort by Finish Date: Not implemented yet, sorry")
+}
+
+module.exports.sortCreateDate = function (req, res){
+    res.send("Sort by Creation Date: Not implemented yet, sorry")
+}
+
+module.exports.sortImportance = function (req, res){
+    res.send("Sort by Importance: Not implemented yet, sorry")
+}
+
+module.exports.hideFinished = function (req, res){
+    res.send("hide Finished: Not implemented yet, sorry")
+}
---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const indexLinks = require('./controller/indexController.js');
+
+/* GET users listing. */
+router.get('/', indexLinks.showIndex);
+
+router.get("/createNote", indexLinks.createNote);
+router.get("/styleSwitch", indexLinks.styleSwitch);
+router.get("/sortFinishDate", indexLinks.sortFinishDate);
+router.get("/sortCreateDate", indexLinks.sortCreateDate);
+router.get("/sortImportance", indexLinks.sortImportance);
+router.get("/hideFinished", indexLinks.hideFinished);
+
+module.exports = router;
