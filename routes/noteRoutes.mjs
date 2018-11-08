fa8298d0441046aedf70cec3b519234c9d87a198
Kuma <michael-naef@hotmail.com>
2018-11-08 17:00:41 +0100

    fixed layout/css not showing

--- routes/noteRoutes.mjs
+++ routes/noteRoutes.mjs
@@ -1,6 +1,5 @@
 import express from 'express';
 const router = express.Router();
-// const editNoteController = require("../controller/editNoteController");
 import {editNoteController} from '../controller/editNoteController.mjs';
 
 router.get("/", editNoteController.showNote.bind(editNoteController));
